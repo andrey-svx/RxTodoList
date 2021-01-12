adef48cfe03f39ba39e6b157515de57f9c5979eb
Andrey Isaev <andron.isaev@gmail.com>
2021-01-13 03:22:09 +0500

    Code refactored

--- RxTodoList/Model/Todo.swift
+++ RxTodoList/Model/Todo.swift
@@ -8,11 +8,18 @@ struct Todo {
     let date: Date
     var objectID: NSManagedObjectID?
     
-    init(_ name: String = "", id: UUID = UUID(), date: Date = Date(), objectID: NSManagedObjectID? = nil) {
+    init(
+        _ name: String = "",
+        id: UUID = UUID(),
+        date: Date = Date(),
+        objectID: NSManagedObjectID? = nil
+    ) {
+        
         self.name = name
         self.id = id
         self.date = date
         self.objectID = objectID
+    
     }
     
     mutating func update(_ name: String) {
